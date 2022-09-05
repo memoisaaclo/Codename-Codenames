48ced444dc02b0a620a05ec414a52fe80e917024
@benckell <benckell@iastate.edu>
2022-09-04 19:25:21 -0500

    testing maven main

---
+++ Experiments/Benjamin/Test1MavenSetup/src/main/java/TestMain.java
@@ -0,0 +1,12 @@
+/**
+ * @author Raine
+ * created 9/4/2022
+ * @project Test1MavenSetup
+ */
+public class TestMain {
+
+    public static void main(String[] args){
+        System.out.println("Hello world. Maven test");
+    }
+
+}
