25094a371aee7077161e05a2047fe1401156f163
@benckell <benckell@iastate.edu>
2022-09-07 11:17:01 -0500

    rename

--- Experiments/Benjamin/Experement1/src/test/java/com/example/demo/WelcomeController.java
+++ Experiments/Benjamin/Experement1/src/test/java/com/example/demo/WelcomeController.java
@@ -9,19 +9,11 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-    	System.out.println("test");
         return "Hello and welcome to COMS 309";
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-    	System.out.println("test /name");
         return "Hello and welcome to COMS 309: " + name;
-    }
-    
-    @GetMapping("/error")
-    public String errorpage() {
-    	System.out.println("test error");
-        return "there was an error";
     }
 }
