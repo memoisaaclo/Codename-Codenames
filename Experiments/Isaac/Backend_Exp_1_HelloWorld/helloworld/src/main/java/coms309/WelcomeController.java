7569b037c2ac5c6b6045e1e650c5838c994a7c61
Isaac Lo <63377487+memoisaaclo@users.noreply.github.com>
2022-09-07 00:41:12 -0500

    Try to do add logic in java controller

--- Experiments/Isaac/Backend_Exp_1_HelloWorld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Isaac/Backend_Exp_1_HelloWorld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -16,4 +16,13 @@ class WelcomeController {
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
     }
+    
+    @GetMapping("/add/{toAdd}")
+    public String welcome(@PathVariable Integer<List> toAdd) {
+    	int total = 0;
+	for (Integer num : toAdd) {
+ 	    total += num;
+	}
+	return total.toString();
+    }
 }
