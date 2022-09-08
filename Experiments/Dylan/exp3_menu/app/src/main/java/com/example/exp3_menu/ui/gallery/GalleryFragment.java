f86e016fbbea23c2b95b6f74ee35079c33a21a34
Dylan <94193563+dbooth05@users.noreply.github.com>
2022-09-07 23:23:04 -0500

    Basic menu

---
+++ Experiments/Dylan/exp3_menu/app/src/main/java/com/example/exp3_menu/ui/gallery/GalleryFragment.java
@@ -0,0 +1,39 @@
+package com.example.exp3_menu.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.exp3_menu.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+    private Button button;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textGallery;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ Experiments/Dylan/exp3_menu/app/src/main/java/com/example/exp3_menu/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.exp3_menu.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
