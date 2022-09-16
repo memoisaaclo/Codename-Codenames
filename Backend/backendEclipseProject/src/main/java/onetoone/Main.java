60d497bd1aaf00f9ff06281a2caa931191821025
@benckell <benckell@iastate.edu>
2022-09-16 18:05:12 -0500

    created project setup

---
+++ Backend/backendEclipseProject/src/main/java/onetoone/Main.java
@@ -0,0 +1,22 @@
+package onetoone;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 
+ * @author Vivek Bengre
+ * 
+ */ 
+
+@SpringBootApplication
+//@EnableJpaRepositories
+class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
