499bd7110cf80351bb5938795f1095236243ec66
Isaac Lo <isaaclo@iastate.edu>
2022-09-21 23:39:32 -0500

    Add primary key to Player. Add PlayerController

--- Backend/backendEclipseProject/src/main/java/onetoone/Users/Player.java
+++ Backend/backendEclipseProject/src/main/java/onetoone/Users/Player.java
@@ -3,9 +3,20 @@ package onetoone.Users;
  * Author: Isaac Lo
  */
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
