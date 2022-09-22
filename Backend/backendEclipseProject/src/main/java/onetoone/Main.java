9bf33becef396a4caed3136b149d9822df391596
Isaac Lo <isaaclo@iastate.edu>
2022-09-22 00:10:00 -0500

    Commit Beans

--- Backend/backendEclipseProject/src/main/java/onetoone/Main.java
+++ Backend/backendEclipseProject/src/main/java/onetoone/Main.java
@@ -4,14 +4,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Date;
 
 /**
  * 
- * @author Vivek Bengre
+ * @author benckell and isaaclo
  * 
  */ 
 
 @SpringBootApplication
+@EnableSwagger2
 //@EnableJpaRepositories
 class Main {
 
@@ -19,4 +27,26 @@ class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
+    CommandLineRunner initUser() {
+        return args -> {
+            /*User user1 = new User("John", "john@somemail.com", new Date())
+            userRepository.save(user3);*/
+        };
+    }
+
+    /* "This Bean creates the documentation for all the APIs Which is exactly
+       what is needed."
+       - Some knowledgeable soul
+       "He he, Bean."
+       - Isaac Lo
+     */
+    @Bean
+    public Docket getAPIdocs() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
