7b8bba5ffc7361b6e6e094651756908ed13698df
Dylan <94193563+dbooth05@users.noreply.github.com>
2022-09-30 21:25:57 -0500

    registration screen development

--- Frontend/Screens/app/src/main/java/com/example/screens/MainActivity.java
+++ Frontend/Screens/app/src/main/java/com/example/screens/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.register2);
+
+        //button click listener
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, registration.class));
     }
 }
