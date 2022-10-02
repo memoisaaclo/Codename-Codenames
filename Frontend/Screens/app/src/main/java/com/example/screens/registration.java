6d07b21dfe3f600cc6b6abf55fd5417690fde11c
Dylan <94193563+dbooth05@users.noreply.github.com>
2022-10-02 13:12:25 -0500

    registration screens work

---
+++ Frontend/Screens/app/src/main/java/com/example/screens/registration.java
@@ -0,0 +1,21 @@
+package com.example.screens;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+public class registration extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
