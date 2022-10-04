754625a95c4962fcc971ddc97d9dcc770bfe7e4f
Jimmy Driskell <jdriskel@iastate.edu>
2022-10-04 01:00:16 -0500

    Extra screens implemented

--- Frontend/Jimmy/JimmyScreens/app/src/main/java/com/example/codenamesscreens/MainActivity.java
+++ Frontend/Jimmy/JimmyScreens/app/src/main/java/com/example/codenamesscreens/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.codenamesscreens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNavToLobby1 = (Button) findViewById(R.id.button_lobby1);
+
+        btnNavToLobby1.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LobbyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
