f1b5726dbcfe80b18a2634014e886d56cb9f65ea
Isaac Lo <isaaclo@iastate.edu>
2022-10-04 18:32:10 -0500

    add untracked files

--- Backend/backendEclipseProject/src/main/java/onetoone/Users/Player.java
+++ Backend/backendEclipseProject/src/main/java/onetoone/Users/Player.java
@@ -10,13 +10,43 @@ public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private int id;
 
-    public Long getId() {
+    private boolean active;
+
+    private Integer gamesPlayed;
+
+    private Integer gamesWon;
+
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Integer getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(Integer gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public Integer getGamesWon() {
+        return gamesWon;
+    }
+
+    public void setGamesWon(Integer gamesWon) {
+        this.gamesWon = gamesWon;
     }
 }
