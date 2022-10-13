1d6e3d103882a90d227f83062865a37ec6450b85
Isaac Lo <isaaclo@iastate.edu>
2022-10-13 15:15:13 -0500

    Added card funcitonality

--- Backend/springbootApplication/src/main/java/codenames/Card.java
+++ Backend/springbootApplication/src/main/java/codenames/Card.java
@@ -7,4 +7,28 @@ public class Game implements Serializable {
     private String word;
 
     private boolean revealed;
+
+    public Game() {
+    }
+
+    public Game(String word, boolean revealed) {
+        this.word = word;
+        this.revealed = revealed;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
 }
