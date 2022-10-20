1790f7a1f5795b7b355d2081fc9cafff65764770
Jimmy Driskell <jdriskel@iastate.edu>
2022-10-20 12:29:18 -0500

    Updated Hub

--- Frontend/Codenames/app/src/main/java/com/example/codenames/HubActivity.java
+++ Frontend/Codenames/app/src/main/java/com/example/codenames/HubActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class HubActivity extends AppCompatActivity
+public class HubActivity extends AppCompatActivity implements OnClickListener
 {
+    private Button btnLobby1,btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,15 +17,26 @@ public class HubActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
 
-        Button btnNavToLobby1 = (Button) findViewById(R.id.button_lobby1);
+        Button btnLobby1 = (Button) findViewById(R.id.button_lobby1);
+        Button btnExit = (Button) findViewById(R.id.reg_exit6);
 
-        btnNavToLobby1.setOnClickListener(new View.OnClickListener()
+        btnLobby1.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
         {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(HubActivity.this, LobbyActivity.class);
-                startActivity(intent);
-            }
-        });
+            case R.id.button_lobby1:
+                startActivity(new Intent(HubActivity.this, LobbyActivity.class));
+                break;
+            case R.id.reg_exit6:
+                startActivity(new Intent(HubActivity.this, menu.class));
+                break;
+            default:
+                break;
+        }
     }
 }
