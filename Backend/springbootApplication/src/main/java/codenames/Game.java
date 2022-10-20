d8c8e814e58dfcebf710cdf2278c037ad2ef1f0a
Isaac Lo <isaaclo@iastate.edu>
2022-10-20 00:33:43 -0500

    Clean up file by moving around methods and adding some commentsadsfasdadfs

--- Backend/springbootApplication/src/main/java/codenames/Game.java
+++ Backend/springbootApplication/src/main/java/codenames/Game.java
@@ -7,11 +7,24 @@ import java.util.List;
 
 @Entity
 public class Game implements Serializable {
-
+    /*
+     * Field variables *
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private int id;
+
+    @Column(name = "clues")
+    private String clues = "";
+
+    @Column(name = "moves")
+    private String moves;
+
+    @Column(name = "gameLobbyName", unique = true)
+    private String gameLobbyName;
+    @OneToMany(orphanRemoval = false, fetch = FetchType.EAGER)
+    private List<Player> players = new ArrayList<Player>();
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "id")
     @JoinTable(
@@ -21,78 +34,20 @@ public class Game implements Serializable {
     )
     private ArrayList<Card> cards = new ArrayList<>();
 
-    @Column(name = "clues")
-    private String clues = "";
-
-    @Column(name = "moves")
-    private String moves;
-
-    @OneToMany(orphanRemoval = false, fetch = FetchType.EAGER)
-    private List<Player> players = new ArrayList<Player>();
+    /*
+     * Constructors *
+     */
+    public Game() {
+    }
 
     public Game(int id, String gameLobbyName) {
         this.id = id;
         this.gameLobbyName = gameLobbyName;
     }
 
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    public void addPlayer(Player player) {
-        this.players.add(player);
-    }
-
-    public void setPlayers(List<Player> players) {
-        this.players = players;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMoves() {
-        return moves;
-    }
-
-    public void setMoves(String moves) {
-        this.moves = moves;
-    }
-
-    public Game() {
-    }
-
-    @Column(name = "gameLobbyName", unique = true)
-    private String gameLobbyName;
-
-    public ArrayList<Card> getCards() {
-        return cards;
-    }
-
-    public void setCards(ArrayList<Card> cards) {
-        this.cards = cards;
-    }
-
-    public String getGameLobbyName() {
-        return gameLobbyName;
-    }
-
-    public void setGameLobbyName(String gameLobbyName) {
-        this.gameLobbyName = gameLobbyName;
-    }
-
-    public String getClues() {
-        return clues;
-    }
-
-    public void setClues(String clues) {
-        this.clues = clues;
-    }
-
+    /*
+     * Special methods *
+     */
     /**
      * Add clue to list of clues
      * Clue should be in the specified form:
@@ -102,4 +57,22 @@ public class Game implements Serializable {
     public void addClue(String clue) {
         this.clues += " " +  clue.strip();
     }
+
+    public void addPlayer(Player player) { this.players.add(player); }
+
+    /*
+     * Getters and setters *
+     */
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getMoves() { return moves; }
+    public void setMoves(String moves) { this.moves = moves; }
+    public ArrayList<Card> getCards() { return cards; }
+    public void setCards(ArrayList<Card> cards) { this.cards = cards; }
+    public String getGameLobbyName() { return gameLobbyName; }
+    public void setGameLobbyName(String gameLobbyName) { this.gameLobbyName = gameLobbyName; }
+    public List<Player> getPlayers() { return players; }
+    public void setPlayers(List<Player> players) { this.players = players; }
+    public String getClues() { return clues; }
+    public void setClues(String clues) { this.clues = clues; }
 }
