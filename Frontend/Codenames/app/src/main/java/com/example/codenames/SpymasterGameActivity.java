397441d9913b5b767fd2b30b8a90f4a94b8bdb30
Jimmy Driskell <jdriskel@iastate.edu>
2022-10-22 19:57:14 -0500

    Preparing cards

--- Frontend/Codenames/app/src/main/java/com/example/codenames/SpymasterGameActivity.java
+++ Frontend/Codenames/app/src/main/java/com/example/codenames/SpymasterGameActivity.java
@@ -7,10 +7,41 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpymasterGameActivity extends AppCompatActivity implements OnClickListener
 {
     private Button btnExit;
-    private Button[] cards;
+
+    private List<Button> cards;
+    private static final int[] CARD_IDS = {
+        R.id.button_card1,
+        R.id.button_card2,
+        R.id.button_card3,
+        R.id.button_card4,
+        R.id.button_card5,
+        R.id.button_card6,
+        R.id.button_card7,
+        R.id.button_card8,
+        R.id.button_card9,
+        R.id.button_card10,
+        R.id.button_card11,
+        R.id.button_card12,
+        R.id.button_card13,
+        R.id.button_card14,
+        R.id.button_card15,
+        R.id.button_card16,
+        R.id.button_card17,
+        R.id.button_card18,
+        R.id.button_card19,
+        R.id.button_card20,
+        R.id.button_card21,
+        R.id.button_card22,
+        R.id.button_card23,
+        R.id.button_card24,
+        R.id.button_card25,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +52,17 @@ public class SpymasterGameActivity extends AppCompatActivity implements OnClickL
         Button btnExit = (Button) findViewById(R.id.reg_exit8);
 
         btnExit.setOnClickListener(this);
+
+        //Cards
+
+        cards = new ArrayList<Button>(CARD_IDS.length);
+        
+        for(int id : CARD_IDS)
+        {
+            Button card = (Button)findViewById(id);
+            card.setOnClickListener(this);
+            cards.add(card);
+        }
     }
 
     @Override
