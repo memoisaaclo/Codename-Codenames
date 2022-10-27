5e6bdf9a9bbf50d6176f39e8fea88d1be7dd603b
@benckell <benckell@iastate.edu>
2022-10-26 21:46:41 -0500

    annotations

--- Backend/springbootApplication/src/main/java/codenames/AdminController.java
+++ Backend/springbootApplication/src/main/java/codenames/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,25 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+	@SuppressWarnings("unused")
 	private String success = "{\"message\":\"success\"}";
-    private String failure = "{\"message\":\"failure\"}";
+    @SuppressWarnings("unused")
+	private String failure = "{\"message\":\"failure\"}";
 	
 	public AdminController() {
 		
 	}
 	
 	@GetMapping(path = "/admin/cards/all")
-    List<Card> getAllCards(){
+	public @ResponseBody List<Card> getAllCards(){
         return Main.cardRepo.findAll();
     }
 	
 	@PutMapping(path = "/admin/cards/add")
-	void addCard(@RequestBody Card card) {
+	public void addCard(@RequestBody Card card) {
 		Main.cardRepo.save(card);
 	}
 	
 	@DeleteMapping(path = "/admin/cards/remove")
-	void removeCard(@RequestBody Card card) {
+	public void removeCard(@RequestBody Card card) {
 		Main.cardRepo.deleteByword(card.getWord());
 	}
 	
