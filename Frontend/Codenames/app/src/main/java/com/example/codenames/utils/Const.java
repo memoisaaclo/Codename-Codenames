22159cb5f7d5a1e1e126589ad73742673b020941
Dylan <94193563+dbooth05@users.noreply.github.com>
2022-11-03 14:52:56 -0500

    const cleanup

--- Frontend/Codenames/app/src/main/java/com/example/codenames/utils/Const.java
+++ Frontend/Codenames/app/src/main/java/com/example/codenames/utils/Const.java
@@ -4,44 +4,37 @@ public class Const {
 
 	// Registration Endpoint
 	public static final String URL_JSON_REGISTRATION = "/users/register/";
-	// 	public static final String URL_JSON_REGISTRATION = "/users/register/";
 
 	// Login Endpoint
 	public static final String URL_JSON_LOGIN = "/users/login";
-	// public static final String URL_JSON_LOGIN = "/users/login/";
-
-	// Get number players in lobby Endpoint
-	public static final String URL_JSON_PLAYERNUM_GET = "http://10.90.75.56:8080/games/3/numPlayers";
 
 	// Add the game id in between parts of URL
 	public static final String URL_JSON_PLAYERNUM_GET_FIRST = "http://10.90.75.56:8080/games/";
 	public static final String URL_JOSN_PLAYERNUM_GET_SECONd = "/numPlayers";
 
-
-	// ^ "http://10.90.75.56:8080/games/3/numPlayers";
-	// ^ "https://4d9722ab-1bce-484c-8966-0096b24a4e15.mock.pstmn.io/post_test/";
-
-
-	public static final String URL_JSON_PLAYERNUM_POST = "http://10.90.75.56:8080/games/3/addPlayer/4";
-
 	// Add the game id in between parts of URL and then playerID after second
 	public static final String URL_JSON_PLAYERNUM_POST_FIRST = "/games/";
 	public static final String URL_JSON_PLAYERNUM_POST_SECOND = "/addplayer/";
 
-	// ^ "http://10.90.75.56:8080/games/3/addPlayer?player_id=3";
-	// ^ "https://4d9722ab-1bce-484c-8966-0096b24a4e15.mock.pstmn.io/post_test/";
-
+	// Gets lobby name
 	public static final String URL_JSON_GAMELOBBYNAME_GET = "http://10.90.75.56:8080/games/";
 
+	// Gets user statistics
 	public static final String URL_JSON_STATISTICS = "http://10.90.75.56:8080/users/";
 
+	// Gets Lobby Info
 	public static final String URL_JSON_LOBBY = "http://10.90.75.56:8080/games/lobbyinfo";
 
+	// Creates Game
 	public static final String URL_JSON_CREATE = "http://10.90.75.56:8080/games/add";
 
+	// Adds Card/Word
 	public static final String URL_JSON_WORD_ADD = "/admin/cards/add";
+
+	// Deletes Card/Word
 	public static final String URL_JSON_WORD_DELETE = "/admin/cards/remove";
 
+	// Gets Clue
 	public static final String URL_JSON_CLUE_GET = "http://10.90.75.56:8080/games/38/clue";
 	public static final String URL_JSON_CLUE_PUT = "/games/38/clue/testing/123";
 
