6a29c0d0de2b47e3690f993ebcad613c9cd47b3f
Isaac Lo <isaaclo@iastate.edu>
2022-11-08 15:42:53 -0600

    Add messsage class and websocket config files

---
+++ Backend/src/main/java/codenames/Message.java
@@ -0,0 +1,8 @@
+public class Message {
+
+    private String from;
+    private String text;
+
+    // getters and setters
+}
+
---
+++ Backend/src/main/java/codenames/WebSocketConfig.java
@@ -0,0 +1,16 @@
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+         registry.addEndpoint("/chat");
+         registry.addEndpoint("/chat").withSockJS();
+    }
+}
